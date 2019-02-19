f84a04fae9084dea07d42b8a7e1976acb93364c0
zhufeng <zhufeng_solo@163.com>
2019-02-19 16:51:56 +0800

    add sass

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="hello">
+  <div class="login-container">
+    <div class="avatar">
+      <img alt="Vue logo" src="../assets/logo.png" style="width: 100px">
+      <div class="user-name">name</div>
+    </div>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -31,28 +35,41 @@
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'Login',
+        props: {
+            msg: String
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .login-container{
+    padding: 16px;
+
+  }
+  .avatar {
+    margin-top: 10px;
+    text-align: center;
+  }
+  .user-name {
+    text-align: center;
+    font-size: 14px;
+    font-weight: 600;
+  }
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
 </style>
